6c676bd7df5c895cd84e54802446e4a132d0cd4f
Nobleman20 <emmanueloluyege@gmail.com>
2023-07-11 16:29:20 +0100

    home dynamic code

--- test/js/productData.js
+++
@@ -1,7 +0,0 @@
-const products = [
-  { image: "Images/c-d-x-PDX_a_82obo-unsplash.jpg", name: "Wireless PC Headset", price: 50000},
-  { image: "Images/c-d-x-PDX_a_82obo-unsplash.jpg", name: "Wireless PC Headset", price: 50000},
-  { image: "Images/c-d-x-PDX_a_82obo-unsplash.jpg", name: "Wireless PC Headset", price: 50000}
-];
-
-export default products;
--- test/js/productsElements.js
+++ test/js/productsElements.js
@@ -12,8 +12,19 @@ const products = [
   { image: "Images/mac-mini-hero-202301_FMT_WHH.jpeg", name: "Imac Mini", price: null},
   { image: "Images/desktop.jpeg", name: "Desktops", price: null},
   { image: "Images/gaming pad.jpeg", name: "Gaming pad", price: 25000},
-  { image: "Images/v-a-tao-KGB8Ctwss90-unsplash.jpg", name: "Smart watch for Apple", price: 75000},
-  { image: "Images/v-a-tao-KGB8Ctwss90-unsplash.jpg", name: "Smart watch for Apple", price: 75000}
+  { image: "Images/iphone.jpeg", name: "Iphone 12 pro max for Apple", price: 450000},
+  { image: "Images/Apple_watch-experience-for-entire-family-hero_09152020_big.jpg.large.jpg", name: "Smart watch for ios", price: 75000},
+  { image: "Images/AirPods-Android-featured-image.jpg", name: "Airpod for android ", price: 25000},
+  { image: "Images/Ps4-slim.jpeg", name: "Ps4 Slim", price: 20000},
+  { image: "Images/JBLspeaker.jpeg", name: "JBL Speaker", price: 50000},
+  { image: "Images/macbook-pro.jpeg", name: "Apple MacBook", price: 500000},
+  { image: "Images/Apple-Magic-Mouse-2-A1657.png", name: "Apple Wireless Mouse", price: 25000},
+  { image: "Images/apple-ipad-pro.jpeg", name: "Apple ipad pro", price: 250000},
+  { image: "Images/quaritsch-photography-m2zuB8DqwyM-unsplash.jpg", name: "Imac", price: 600000},
+  { image: "Images/Apple-Watch-SE-aluminum-midnight-220907_inline.jpg.large.jpg", name: "Apple Watch", price: 7500000},
+  { image: "Images/apple-airpod.jpeg", name: "Apple earpod", price: 25000},
+  { image: "Images/12-pro-max.jpg", name: "Iphone 12 Pro Max", price: 300000},
+  { image: "Images/apple-airpod.jpg", name: "Apple Airpod Max", price: 150000}
 ];
 
 const containers = document.querySelectorAll("#product-container");
@@ -28,12 +39,26 @@ products.forEach((product, index) => {
     <section>
       <img src="${product.image}" alt="${product.name}">
       <h3>${product.name}</h3>
-      ${formattedPrice ? `<span>${formattedPrice}</span>` : ''}
-      ${index >= 12 && index <= 15 ? '<button class="add-to-cart">Add to Cart</button>' : ''}
+      ${formattedPrice ? `<span class="pricedata">${formattedPrice}</span>` : ''}
+      ${index >= 12 ? '<br><button class="add-to-cart">Add to Cart</button>' : ''}
     </section>
   `;
+
+  const addToCartButtons = productElement.querySelectorAll(".add-to-cart");
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    openAddToCartWindow(product);
+  });
+});
+
+function openAddToCartWindow(product) {
+  // Perform the logic to open the "Add to Cart" window
+  // For example, you can show a modal or trigger a pop-up
+  console.log(`Product "${product.name}" - Add to Cart window opened.`);
+}
 
   containers.forEach(container => {
     container.appendChild(productElement.cloneNode(true));
   });
 });
+
